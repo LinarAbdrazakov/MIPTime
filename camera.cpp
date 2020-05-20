0c472ae3a0b1c4024a2ee6421bdfe96521104062
LinarAbdrazakov <linar200015@gmail.com>
2020-05-20 22:43:43 +0500

    basic version of camera class

---
+++ camera.cpp
@@ -0,0 +1,30 @@
+#include "camera.h"
+
+
+Camera::Camera() {
+	open();
+}
+
+int Camera::open() {
+	if(!cap.open(0)) return -1;
+	return 0;
+}
+
+cv::Mat Camera::get_frame() {
+	cv::Mat frame;
+	cap >> frame;
+
+	return frame;
+}
+
+std::vector <uchar> Camera::get_jpeg_frame() {
+	cv::Mat frame = get_frame();
+
+	std::vector <uchar> buff;                  //buffer for coding
+	std::vector <int> param(2);
+	param[0] = cv::IMWRITE_JPEG_QUALITY;
+	param[1] = 30;                             //default(95) 0-100
+	cv::imencode(".jpg", frame, buff, param);
+
+	return buff;
+}
---
+++ camera.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include "opencv2/opencv.hpp"
+
+
+// Class for camera
+class Camera {
+private:
+	cv::VideoCapture cap;
+
+public:
+	Camera();
+	int open();
+	cv::Mat get_frame();
+	std::vector <uchar> get_jpeg_frame();
+};
